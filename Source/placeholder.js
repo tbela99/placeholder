b66fafb67ff1deeb3bfcabf4dce9ce27e73bda7d
Thierry Bela Nanga <bntfr@yahoo.fr>
2011-03-25 11:40:48 +0100

    preserve original color when the placeholder is applied more than once

--- Source/placeholder.js
+++ Source/placeholder.js
@@ -25,18 +25,21 @@ provides: none
 			var key = 'placeholder',
 				evt = 'ph:events',
 				key_c = 'ph:color',
+				key_sc = 'ph:scolor',
 				self = this,
 				isNative = key in document.createElement('input'),
-				color,
+				color = this.retrieve(key_sc, this.style.color),
 				events,
 				form = $(self.form),
-				params = Array.link(arguments, {options: Object.type, value: $defined}),
+				params = Array.link(arguments, {options: Object.type, value: function (obj) { return obj != null }}),
 				placeholder = params.value || self.retrieve(key),
 				options = params.options || {},
 				pcolor = options.color || self.retrieve(key_c, '#aaa');
 				
 			if(placeholder == undefined) placeholder = self.defaultValue;
 			self.store(key_c, pcolor).store(key, placeholder);
+			
+			this.style.color = color;
 			
 			//fix the reset button
 			if(!form.retrieve(key)) form.store(key, true).addEvent('submit', function () { form.getElements('.placeholder').each(focus) });
